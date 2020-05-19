d3256ef0db4bcc1c132dc78aa25435926d10903c
Nathan Chen <nathan@Nathans-MacBook-Pro.local>
2020-05-18 19:02:51 -0700

    fixed grid alignment

--- js/buildProductGrid.js
+++ js/buildProductGrid.js
@@ -1,12 +1,6 @@
-// incrementing variable for unique ids and parsing
-var cardNum = 0;
-const products = window.sessionStorage.getItem("queryResult");
+const products = JSON.parse(window.sessionStorage.getItem("queryResult"));
 
-function test() { 
-    console.log("Test function working")
-}
-
-function setupCard(product, rowNum) {
+function setupCard(product, cardNum) {
     // <div class="col-lg-6 col-md-6">
     //     <div class="single-product-item">
     //         <a href="./product-page.html">
@@ -21,15 +15,13 @@ function setupCard(product, rowNum) {
     //     </div>
     // </div>
 
-    const div_outer = document.createElement('div')
-    div_outer.setAttribute('class', 'col-lg-6 col-md-6') 
-
     const div_singleProd = document.createElement('div')
     div_singleProd.setAttribute('class', "single-product-item")
+    div_singleProd.setAttribute('style', "position: relative; width: 25%; padding-right: 15px; padding-left: 15px;")
 
     const a_img = document.createElement('a')
     a_img.setAttribute('id', 'a_img_href' + cardNum)
-    a_img.setAttribute('href', './product-page.html')
+    a_img.setAttribute('href', './product-page.html?ProductId=' + product.ProductId)
 
     const figure = document.createElement('figure')
 
@@ -43,12 +35,11 @@ function setupCard(product, rowNum) {
     const a_product = document.createElement('a')
     // TODO: Need to customize product page link 
     a_product.setAttribute('id', "a_product_href" + cardNum)
-    a_product.setAttribute('href', "./product-page.html")
+    a_product.setAttribute('href', "./product-page.html?ProductId=" + product.ProductId)
 
     const h6_product = document.createElement('h6')
     h6_product.innerHTML = product.Name
 
-    div_outer.append(div_singleProd)
     div_singleProd.append(a_img)
     a_img.append(figure)
     figure.append(img)
@@ -57,41 +48,14 @@ function setupCard(product, rowNum) {
     div_prodText.append(a_product)
     a_product.append(h6_product)
 
-    $("#rowContainer2" + rowNum).append(div_outer);
+    $("#productgrid").append(div_singleProd);
     
-    console.log("setupCard end")
-	// });
 }
 
 function setupGrid() {
-    var entriesPerRow = 4;
-    var totalRows = products.length % 4;
-
-    console.log("Outside forloop")
-
-    for (let rowNum = 1; rowNum <= totalRows; rowNum++) { 
-        // Append new row-div to the overarching div (id "productgrid")
-        const rowContainer1 = document.createElement('div')
-        rowContainer1.setAttribute('class', 'col-lg-6 col-md-6')
-        rowContainer1.setAttribute('id', 'rowContainer1' + rowNum)
-
-        const rowContainer2 = document.createElement('div');
-        rowContainer2.setAttribute('class', 'row')
-        rowContainer2.setAttribute('id', 'rowContainer2' + rowNum)
-
-        rowContainer1.append(rowContainer2)
-        $("#productgrid").append(rowContainer2)
-
-        // Set up productsPerRow cards per row 
-        // for (let rowEntryNum = 1; rowEntryNum <= entriesPerRow && cardNum < products.size; rowEntryNum++) { 
-        for (let rowEntryNum = 1; rowEntryNum <= entriesPerRow && cardNum < products.length; rowEntryNum++) { 
-            console.log("In entry forloop")
-            setupCard(products[cardNum], rowNum)
-            cardNum++;
-        }
+    for (let cardNum = 0; cardNum < products.length; cardNum++) { 
+        setupCard(products[cardNum], cardNum)
     }
-
-    console.log("End of grid method")
-
 }
+
 setupGrid()
