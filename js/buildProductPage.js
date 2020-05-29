bc7c1fedf51da81d501fca5e65488a74f22b8754
Nathan Chen <nathan@Nathans-MacBook-Pro.local>
2020-05-28 20:00:08 -0700

    facebook comments testing

--- js/buildProductPage.js
+++ js/buildProductPage.js
@@ -135,6 +135,11 @@ function setupProduct(product) {
         $('#features-product').append("<li tabindex='0'>" + validFeatures[key] + ": " + product[key] + "</li>")
     }) 
 
+    $("autofbcomments").append("<div class='fb-comments' data-href='https://assistivetechlib.herokuapp.com/product-page.html?ProductId=" + product.ProductId + " data-numposts='7' data-width=''></div>");
+
+    
+
+
 }
 
 setupProduct()
